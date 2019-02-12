17ae568993c2c650a61a8397f5198907e27a957a
markimuu <kuutokm@gmail.com>
2019-02-12 21:39:14 +0200

    tietokanta connection lisätty

---
+++ connect.php
@@ -0,0 +1,23 @@
+<?php
+fun
+ction connect_db() {
+$servername = "localhost";
+$username = "a1600507";
+$password = "weXEkE52i";
+$dbname = "a1600507";
+// Create connection
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn
+-
+>connect_er
+ror) {
+die("Connection failed: " . $conn
+-
+>connect_error);
+} else {
+}
+return $conn;
+}
+?>
